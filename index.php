ced3669557d8deed297ace7800317e1ce89897b8
Cheruto_yego <107578924+cheruto-yego@users.noreply.github.com>
2023-11-05 15:41:27 +0300

    index and signup changed names

--- index.php
+++ index.php
@@ -3,16 +3,22 @@ session_start();
 error_reporting(0);
 include('includes/db_connection.php');
 error_reporting(0);
+
+if (isset($_POST['submit'])){
+    $Enter_email = $_POST['Enter_email'];
+    $Enter_name = $_POST['Enter_name'];
+    $Enter_number = $_POST['Enter_number'];
+    $password = md5($_POST['password']);
+    
+    $query = mysqli_query($connection,"insert into client_table (email, username, password,client_number) VALUES ('$Enter_email','$Enter_name','$password','$Enter_number')");
+    if ($query) {
+        echo "<script> alert('User entered successfully');</script>";
+    }
+    else{
+        echo "<script> alert('User entered successfully');</script>";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Landing Page</title>
-</head>
-<body>
-    <!-- <button href = "signup.php">Signup</button> -->
-    <a href = "signup.php">Signup</a>
-</body>
-</html>
